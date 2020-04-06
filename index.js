fbdecaae3d524c226eb2dd0a3093b2662f61d552
Impalez <38750060+Impalez@users.noreply.github.com>
2020-04-06 13:30:43 +0300

    Music Bot
    -Play a song
    -Skip
    *Need bugfix for queue and stop*

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NjkyMzQ3MTI5NTQwMTE2NTIx.XntRwA._RktAbUmEwQpPcYHMiH-H0yeWKg';
+const token = 'NjkyMzQ3MTI5NTQwMTE2NTIx.Xn4yLQ.sbF5bSkiZH6pc67aAMmdmI_L-ec';
 const ms = require('ms');
 const PREFIX = '!';
 
 
+const ytdl = require("ytdl-core");
+
+var version ='1.2';
+var servers = {};
+
 bot.on('ready', () =>{
     console.log('This bot is online!');
 
-    bot.user.setActivity('La primărie', {type: 'PLAYING'})
+    bot.user.setActivity('Fotbal galactic', {type: 'PLAYING'})
 })
 
 bot.on('message', message=>{
@@ -20,23 +25,17 @@ bot.on('message', message=>{
         case 'ping':
             message.channel.send('pong!')
             break;
+
         case 'info':
-            if(args[1] === 'bobita'){
-                message.channel.send('prost :[')
-            }
-            else 
-            if(args[1] === 'tavi'){
-                message.channel.send('pitic :<')
-            }
-            else{
-                message.channel.send('ca tine!')
-            }
+        
             break;
+
         case 'clear':
             if(!args[1]) return message.reply('ERROR! Clear what?')
             message.channel.bulkDelete(args[1])
             break;
-        case 'embed':
+
+        case 'me':
             const embed = new Discord.MessageEmbed()
             .setTitle('User Information')
             .addField('Player Name:', message.author.username)
@@ -44,21 +43,76 @@ bot.on('message', message=>{
             .setThumbnail(message.author.displayAvatarURL())
             message.channel.send(embed);
             break;
-        case 'mute':
 
+        case 'play':
+
+            function play(connection, message){
+                var server = servers[message.guild.id];
+
+                server.dispatcher = connection.play(ytdl(server.queue[0], {filter: 'audioonly'}));
+
+                server.queue.shift();
+
+                server.dispatcher.on("end", function(){
+                    if(server.queue[0]){
+                        play(connection, message);
+                    } else {
+                        connection.disconnect();
+                    }
+                });
+            }
+
+            if(!args[1]){
+                message.channel.send("You need to provide a link!");
+                return;
+            }
+
+            if(!message.member.voice.channel){
+                message.channel.send("You must be in a channel!");
+                return;
+            }
+
+            if(!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            }
+        
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+            if(!message.guild.voiceConnection) message.member.voice.channel.join().then(function(connection){
+                play(connection, message);
+                message.reply("playing now for you.");
+            })
+
+
+            break;
+
+        case 'skip':
+            var server = servers[message.guild.id];
+
+            if(server.dispatcher) server.dispatcher.end();
+            message.channel.send("Skipping the song")
+            break;
+
+        case 'stop':
+            var server = servers[message.guild.id];
+            if(message.guild.voiceConnection){
+                for(var i = server.queue.length - 1; i >=0; i--){
+                    server.queue.splice(i,1);
+                }
+
+                server.dispatcher.end();
+                message.channel.send("Leaving the voice channel.")
+                console.log('Stopped the queue')
+             }
+
+            if(message.guild.connection) message.guild.voiceConnection.disconnect();
+            break;
 
 
     }
 
-   /* if (message.content === "HELLO"){
-        message.reply('HELLO FRIEND! IM THE MAYOR ')
-        
-    }
-
-    if (msg.content === "Iancule"){
-        msg.reply('N-am tigari!!! ')
-        
-    } */
 
 })
 
